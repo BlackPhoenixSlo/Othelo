9140fa96789bb73221bda3227da47a68072b4060
mojcabozic <82882602+mojcabozic@users.noreply.github.com>
2022-06-13 21:54:31 +0200

    Deleted test code from Inteligenca.java

--- src/gui/Inteligenca.java
+++ src/gui/Inteligenca.java
@@ -9,51 +9,7 @@ import splosno.Poteza;
 
 class Inteligenca extends KdoIgra {
     public static void main(String[] args) {
-        int win1 = 0;
-        int win2 = 0;
-        long dursum = 0;
-        int N = 100;
-        int penisfuckcumlollmaotouchgrass = 0;
-        long max_cas = 0;
-        for(int i = 0; i < N; i++){
-            System.out.println(i);
-            Igra igra1 = new Igra();
-            while (igra1.stanje() == Igra.Stanje.V_TEKU) {
-                if(igra1.getValidMoves().size() == 0){
-                    igra1.igralecNaPotezi = igra1.igralecNaPotezi == 1 ? 2 : 1;
-                }
-                long startTime = System.nanoTime();
-                Koordinati p;
-                if(igra1.igralecNaPotezi == 1){
-                    ArrayList<Koordinati> moznePoteze = igra1.getValidMoves();
-                    p = moznePoteze.get((int)(moznePoteze.size() * Math.random()));
-                }else{
-                    p = izberiPotezo(igra1, 6, -500, 500, igra1.naPotezi()).poteza;
-                }
-                long endTime = System.nanoTime();
-
-                if(igra1.igralecNaPotezi == 2){
-                    dursum += (endTime - startTime);
-                    penisfuckcumlollmaotouchgrass++;
-                    if((endTime - startTime) > max_cas){
-                        max_cas = (endTime - startTime);
-                    }
-                }
-                igra1.odigraj(p);
-                printBoard(igra1.getBoard());
-            }
-
-            if(igra1.stanje() == Igra.Stanje.ZMAGA_1){
-                win1++;
-            }else if(igra1.stanje() == Igra.Stanje.ZMAGA_2){
-                win2++;
-            }
-        }
-
-        System.out.printf("Random je zmagal: %d iger (%d%%)\n", win1, win1 * 100/(win1+win2));
-        System.out.printf("Mojca je zmagala: %d iger (%d%%)\n",win2, win2 * 100/(win1+win2));
-        System.out.printf("Povprecni cas odlocanja: %d ms \n", (int)(dursum/penisfuckcumlollmaotouchgrass ) / 1000);
-        System.out.printf("Max cas odlocanja: %d ms\n", (int) max_cas / 1000);
+        
     }
     
     public Inteligenca() {
